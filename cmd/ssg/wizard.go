42f37325eadd9743f663df3162a526868e6c13e0
Nathan Paul <mail@polarhive.net>
2024-03-28 01:47:48 +0530

    fix/wizard: correct yml path

--- cmd/ssg/wizard.go
+++ cmd/ssg/wizard.go
@@ -3,13 +3,13 @@ package ssg
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io/fs"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"gopkg.in/yaml.v3"
 )
 
 type Config struct {
@@ -65,43 +65,27 @@ func (ws *WizardServer) handleSubmit(w http.ResponseWriter, r *http.Request) {
 	}()
 }
 
-// writes the configuration to file.
+// writes the configuration to file in YAML format.
 func writeConfigToFile(ctx context.Context, config Config) error {
-	existingConfig := make(map[string]string)
-
-	data, err := os.ReadFile("./site/layout/config.yaml")
-	if err != nil && !errors.Is(err, fs.ErrNotExist) {
-		return err
-	}
-
-	err = json.Unmarshal(data, &existingConfig)
-	if err != nil {
-		return err
-	}
-
-	if config.Author != "" {
-		existingConfig["author"] = config.Author
-	}
-	if config.SiteTitle != "" {
-		existingConfig["siteTitle"] = config.SiteTitle
-	}
-	if config.BaseURL != "" {
-		existingConfig["baseURL"] = config.BaseURL
-	}
-
-	newData, err := json.Marshal(existingConfig)
-	if err != nil {
-		return err
-	}
-
-	configFilePath := "./site/layout/config.yaml"
+	configFilePath := "./site/layout/config.yml"
 	if err := os.MkdirAll(filepath.Dir(configFilePath), 0755); err != nil {
 		return err
 	}
 
-	err = os.WriteFile(configFilePath, newData, 0644)
+	file, err := os.Create(configFilePath)
 	if err != nil {
 		return err
 	}
+	defer file.Close()
+
+	// Encode the config into YAML format and write it to the file.
+	yamlConfig, err := yaml.Marshal(&config)
+	if err != nil {
+		return err
+	}
+	if _, err := file.Write(yamlConfig); err != nil {
+		return err
+	}
+
 	return nil
 }
